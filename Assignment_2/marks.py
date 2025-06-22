92f143f4399c259d0be36e539ecad46ac1d4b0e3
Chetendra Singh <neji5072@gmail.com>
2025-06-22 21:21:03 +0530

    Added Assignment 2

---
+++ Assignment_2/marks.py
@@ -0,0 +1,31 @@
+#write a program to take student name,class and marks and
+#find total marks and percentage and
+#print name,class and percentage
+
+
+# personal details
+
+name=input("enter your name:")
+class_name=int(input("enter your class:"))
+
+# subject details
+sum = 0
+subject_list=[]
+marks_list=[]
+for i in range(0,5):
+        sub=input(f"enter {i+1} subject name:")
+        marks=int(input(f"enter {i+1} subject marks:"))
+        sum+=marks
+        subject_list.append(sub)
+        marks_list.append(marks)
+
+
+#calculation of percentage
+result = (sum / 500) * 100
+
+#output
+print("...............")
+print("Name of Student:",name.upper())
+
+print("Class:",class_name)
+print("Percentage:",result)
