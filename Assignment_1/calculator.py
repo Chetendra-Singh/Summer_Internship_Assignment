7dc6c943e74c59cea91e31421a72944dfe252965
Chetendra Singh <neji5072@gmail.com>
2025-06-23 00:12:01 +0530

    Update Code

--- Assignment_1/calculator.py
+++ Assignment_1/calculator.py
@@ -1,26 +1,28 @@
+#Take two input and perform
+#addition,subtraction,multiplication,division and modulus
+#and print them
+
+#inputs
 num1 = float(input("Enter the first number:"))
 num2 = float(input("enter the second number:"))
-print("choose one operation from below:")
-print("addition","subtraction","multiplication","division","modulus","exponential",sep=",")
-a=input("enter the operation you want to perform:")
 
-if a=="addition":
-    sum=num1+num2
-    print(f"Sum of {num1} and {num2} is:", sum)
-elif a=="subtraction":
-    sub = num1 - num2
-    print(f"Subtraction of {num1} and {num2} is:", sub)
-elif a=="multiplication":
-    multi = num1*num2
-    print(f"Multiplication of {num1} and {num2} is:", multi)
-elif a=="division":
-    div = num1/num2
-    print(f"Divison of {num1} and {num2} is:", div)
-elif a=="modulus":
-    mod = num1%num2
-    print(f"Modulus of {num1} and {num2} is:", mod)
-elif a=="exponential":
-    exp = num1**num2
-    print(f"Exponential of {num1} and {num2} is:", exp)
-else:
-    print("Wrong Choice,Please Choose a Valid Choice.")
+# operations
+sum=num1+num2
+sub = num1 - num2
+multi = num1*num2
+div = num1/num2
+mod = num1%num2
+
+#output print
+print(f"Sum of {num1} and {num2} is:", sum)
+
+print(f"Subtraction of {num1} and {num2} is:", sub)
+
+print(f"Multiplication of {num1} and {num2} is:", multi)
+
+print(f"Divison of {num1} and {num2} is:", div)
+
+print(f"Modulus of {num1} and {num2} is:", mod)
+
+
+
